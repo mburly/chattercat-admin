5572dc1a5b84b1c9609e2e318b329a1035925f7c
mburly <mburleson@cfl.rr.com>
2022-08-01 01:40:20 -0400

    getting something started here

---
+++ Admin/Admin/FileRoot.cs
@@ -0,0 +1,8 @@
+﻿namespace Admin
+{
+    public class FileRoot
+    {
+        public static string Root => Directory.GetParent(Directory.GetCurrentDirectory()).Parent.Parent.ToString();
+        public static string Streams => $"{Root}{Path.DirectorySeparatorChar}streams.txt";
+    }
+}

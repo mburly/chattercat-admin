4e4f0613db31e2687eb47130c1d076e410f91a25
mburly <mburleson@cfl.rr.com>
2022-08-06 03:22:39 -0400

    manage emotes work...

--- Admin/Admin/Models/Emote.cs
+++ Admin/Admin/Models/Emote.cs
@@ -10,9 +10,25 @@
         public string Path { get; set; }
         public string Date_Added { get; set; }
         public int Source { get; set; }
-        public string Active { get; set; }
+        public bool Active { get; set; }
 
-        public Emote(int id, string code, string emote_id, int count, string url, string path, string date_added, int source, string active)
+        public Emote(string code, int count, string path)
+        {
+            Code = code;
+            Count = count;
+            Path = path;
+        }
+        public Emote(string code, int count, string url, string path, string date_added, int source, bool active)
+        {
+            Code = code;
+            Count = count;
+            Url = url;
+            Path = path;
+            Date_Added = date_added;
+            Source = source;
+            Active = active;
+        }
+        public Emote(int id, string code, string emote_id, int count, string url, string path, string date_added, int source, bool active)
         {
             Id = id;
             Code = code;
@@ -23,13 +39,6 @@
             Date_Added = date_added;
             Source = source;
             Active = active;
-        }
-
-        public Emote(string code, int count, string path)
-        {
-            Code = code;
-            Count = count;
-            Path = path;
         }
     }
 }
---
+++ Admin/Admin/Pages/Housekeeping/Manage/Emotes.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Admin.Pages.Housekeeping.Manage
+{
+    public class EmotesModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
